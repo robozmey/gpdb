b6dfe82910271effb827abe882d7e575a47c5286
Vladimir Rachkin <vova@kpnn.ru>
2024-10-25 13:06:54 +0300

    Add try_convert template

--- contrib/try_convert/try_convert.c
+++ contrib/try_convert/try_convert.c
@@ -20,5 +20,40 @@ PG_FUNCTION_INFO_V1(try_convert);
 Datum
 try_convert(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_INT32(1);
+	Oid	argtype = get_fn_expr_argtype(fcinfo->flinfo, 0);
+	Datum value_datum = PG_GETARG_DATUM(0);
+	int64 int_value = 0;
+
+	Datum casttype = PG_GETARG_DATUM(1);
+
+	switch (argtype)
+	{
+	case INT2OID:
+	case INT4OID:
+	case INT8OID:
+		int_value = (int64) PG_GETARG_INT64(0);
+
+		switch (casttype)
+		{
+		case INT2OID:
+			PG_RETURN_INT16((int16) int_value);
+		case INT4OID:
+			PG_RETURN_INT32((int32) int_value);
+		case INT8OID:
+			PG_RETURN_INT32((int64) int_value);
+		case FLOAT4OID:
+			PG_RETURN_FLOAT4((float4) int_value);
+		case FLOAT8OID:
+			PG_RETURN_FLOAT8((float8) int_value);
+		
+		default:
+			PG_RETURN_NULL();
+		}
+		break;
+	
+	default:
+		PG_RETURN_NULL();
+	}
+
+	// PG_RETURN_INT32(argtype);
 }
