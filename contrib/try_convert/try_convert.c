f42b76d2db870e5757686783cd034d2f34b60336
Vladimir Rachkin <vova@kpnn.ru>
2024-12-03 17:52:18 +0300

    Redesign try_convert

--- contrib/try_convert/try_convert.c
+++ contrib/try_convert/try_convert.c
@@ -12,48 +12,155 @@ PG_MODULE_MAGIC;
 PG_FUNCTION_INFO_V1(try_convert);
 
 
-Datum
-try_convert_from_pg_cast(Datum value, Oid sourceTypeId, Oid targetTypeId, bool *is_null)
+typedef enum ConversionType
 {
-    HeapTuple tuple;
+	CONVERSION_TYPE_FUNC,
+	CONVERSION_TYPE_RELABEL,
+	CONVERSION_TYPE_VIA_IO,
+	CONVERSION_TYPE_ARRAY,	
+    CONVERSION_TYPE_NONE	
+} ConversionType;
 
-    /* Look in pg_cast */
-    tuple = SearchSysCache2(CASTSOURCETARGET,
-                            ObjectIdGetDatum(sourceTypeId),
-                            ObjectIdGetDatum(targetTypeId));
 
+ConversionType
+find_conversion_way(Oid targetTypeId, Oid sourceTypeId, Oid *funcid)
+{
+	ConversionType result = CONVERSION_TYPE_NONE;
+	HeapTuple	tuple;
+
+	*funcid = InvalidOid;
+
+	/* Perhaps the types are domains; if so, look at their base types */
+	if (OidIsValid(sourceTypeId))
+		sourceTypeId = getBaseType(sourceTypeId);
+	if (OidIsValid(targetTypeId))
+		targetTypeId = getBaseType(targetTypeId);
+
+	/* Domains are always coercible to and from their base type */
+	if (sourceTypeId == targetTypeId)
+		return CONVERSION_TYPE_RELABEL;
+
+	/* SELECT castcontext from pg_cast */
+
+	/* Look in pg_cast */
+	tuple = SearchSysCache2(CASTSOURCETARGET,
+							ObjectIdGetDatum(sourceTypeId),
+							ObjectIdGetDatum(targetTypeId));
+
+	if (HeapTupleIsValid(tuple))
+	{
+		Form_pg_cast castForm = (Form_pg_cast) GETSTRUCT(tuple);
+
+        switch (castForm->castmethod)
+        {
+            case COERCION_METHOD_FUNCTION:
+                *funcid = castForm->castfunc;
+                result = CONVERSION_TYPE_FUNC;
+                break;
+            case COERCION_METHOD_INOUT:
+                result = CONVERSION_TYPE_VIA_IO;
+                break;
+            case COERCION_METHOD_BINARY:
+                result = CONVERSION_TYPE_RELABEL;
+                break;
+            default:
+                elog(ERROR, "unrecognized castmethod: %d",
+                        (int) castForm->castmethod);
+                break;
+        }
+	
+
+		ReleaseSysCache(tuple);
+	}
+	else
+	{
+		/*
+		 * If there's no pg_cast entry, perhaps we are dealing with a pair of
+		 * array types.  If so, and if the element types have a suitable cast,
+		 * report that we can coerce with an ArrayCoerceExpr.
+		 *
+		 * Note that the source type can be a domain over array, but not the
+		 * target, because ArrayCoerceExpr won't check domain constraints.
+		 *
+		 * Hack: disallow coercions to oidvector and int2vector, which
+		 * otherwise tend to capture coercions that should go to "real" array
+		 * types.  We want those types to be considered "real" arrays for many
+		 * purposes, but not this one.  (Also, ArrayCoerceExpr isn't
+		 * guaranteed to produce an output that meets the restrictions of
+		 * these datatypes, such as being 1-dimensional.)
+		 */
+		if (targetTypeId != OIDVECTOROID && targetTypeId != INT2VECTOROID)
+		{
+			Oid			targetElem;
+			Oid			sourceElem;
+
+			if ((targetElem = get_element_type(targetTypeId)) != InvalidOid &&
+			(sourceElem = get_base_element_type(sourceTypeId)) != InvalidOid)
+			{
+				ConversionType elempathtype;
+				Oid			   elemfuncid;
+
+				elempathtype = find_conversion_way(targetElem,
+												   sourceElem,
+												  &elemfuncid);
+				if (elempathtype != CONVERSION_TYPE_NONE &&
+					elempathtype != CONVERSION_TYPE_ARRAY)
+				{
+					*funcid = elemfuncid;
+					if (elempathtype == CONVERSION_TYPE_VIA_IO)
+						result = CONVERSION_TYPE_VIA_IO;
+					else
+						result = CONVERSION_TYPE_ARRAY;
+				}
+			}
+		}
+
+		/*
+		 * If we still haven't found a possibility, consider automatic casting
+		 * using I/O functions.  We allow assignment casts to string types and
+		 * explicit casts from string types to be handled this way. (The
+		 * CoerceViaIO mechanism is a lot more general than that, but this is
+		 * all we want to allow in the absence of a pg_cast entry.) It would
+		 * probably be better to insist on explicit casts in both directions,
+		 * but this is a compromise to preserve something of the pre-8.3
+		 * behavior that many types had implicit (yipes!) casts to text.
+		 */
+		if (result == CONVERSION_TYPE_NONE)
+		{
+			if (TypeCategory(targetTypeId) == TYPCATEGORY_STRING)
+				result = CONVERSION_TYPE_VIA_IO;
+			else if (TypeCategory(sourceTypeId) == TYPCATEGORY_STRING)
+				result = CONVERSION_TYPE_VIA_IO;
+		}
+	}
+
+	return result;
+}
+
+
+Datum
+try_convert_from_function(Datum value, Oid funcId, bool *is_failed)
+{
     Datum res = 0;
 
-    if (HeapTupleIsValid(tuple))
+    PG_TRY();
     {
-        /* SELECT castcontext from pg_cast */
-        Form_pg_cast castForm = (Form_pg_cast) GETSTRUCT(tuple);
-        CoercionContext castcontext;
-
-        Oid funcId = castForm->castfunc;
-
-        PG_TRY();
-        {
-            res = OidFunctionCall1(funcId, value);
-        }
-        PG_CATCH();
-        {
-            *is_null = true;
-            FlushErrorState();  /// TODO replace
-        }
-        PG_END_TRY();
-
-        ReleaseSysCache(tuple);
-    } else {
-        *is_null = true;
+        res = OidFunctionCall1(funcId, value);
     }
+    PG_CATCH();
+    {
+        *is_failed = true;
+        FlushErrorState();  /// TODO replace
+    }
+    PG_END_TRY();
+
 
     return res;
 }
 
 
 Datum
-try_convert_via_io(Datum value, Oid sourceTypeId, Oid targetTypeId, bool *is_null)
+try_convert_via_io(Datum value, Oid sourceTypeId, Oid targetTypeId, bool *is_failed)
 {
     FmgrInfo *outfunc;
     FmgrInfo *infunc;
@@ -61,6 +168,12 @@ try_convert_via_io(Datum value, Oid sourceTypeId, Oid targetTypeId, bool *is_nul
     Oid iofunc = InvalidOid;
 	bool outtypisvarlena = false;
     Oid intypioparam = InvalidOid;
+
+    /* Perhaps the types are domains; if so, look at their base types */
+	if (OidIsValid(sourceTypeId))
+		sourceTypeId = getBaseType(sourceTypeId);
+	if (OidIsValid(targetTypeId))
+		targetTypeId = getBaseType(targetTypeId);
 
     /* lookup the input type's output function */
     getTypeOutputInfo(sourceTypeId, &iofunc, &outtypisvarlena);
@@ -74,11 +187,8 @@ try_convert_via_io(Datum value, Oid sourceTypeId, Oid targetTypeId, bool *is_nul
 
 	PG_TRY();
 	{
-
-        if (*is_null)
-            string = NULL;			/* output functions are not called on nulls */
-        else
-            string = OutputFunctionCall(&outfunc, value);
+        // value cannot be null
+        string = OutputFunctionCall(&outfunc, value);
 
         res = InputFunctionCall(&infunc,
                                 string,
@@ -87,7 +197,7 @@ try_convert_via_io(Datum value, Oid sourceTypeId, Oid targetTypeId, bool *is_nul
     }
 	PG_CATCH();
 	{
-        *is_null = true;
+        *is_failed = true;
 
         FlushErrorState(); /// TODO replace
 	}
@@ -106,34 +216,48 @@ try_convert(PG_FUNCTION_ARGS)
 
     Oid sourceTypeId = get_fn_expr_argtype(fcinfo->flinfo, 0);
     Oid targetTypeId = get_fn_expr_argtype(fcinfo->flinfo, 1);
-        
-    /* Perhaps the types are domains; if so, look at their base types */
-	if (OidIsValid(sourceTypeId))
-		sourceTypeId = getBaseType(sourceTypeId);
-	if (OidIsValid(targetTypeId))
-		targetTypeId = getBaseType(targetTypeId);
+
+    Oid funcid;
+
+    ConversionType conversion_type = find_conversion_way(targetTypeId, sourceTypeId, &funcid);
 
     Datum value = fcinfo->arg[0];
     Datum res = 0;
 
-    	/* Domains are always coercible to and from their base type */
-	if (sourceTypeId == targetTypeId)
-		return value;
+    bool is_failed = false;
 
-    bool is_null = fcinfo->isnull;
+    switch (conversion_type)
+    {
+    case CONVERSION_TYPE_RELABEL:
+        return value;
 
-    res = try_convert_from_pg_cast(value, sourceTypeId, targetTypeId, &is_null);
+    case CONVERSION_TYPE_FUNC:
+        res = try_convert_from_function(value, funcid, &is_failed);
+        break;
 
-    if (!is_null)
-        return res;
+    case CONVERSION_TYPE_VIA_IO:
+        res = try_convert_via_io(value, sourceTypeId, targetTypeId, &is_failed);
+        break;
 
-    if (TypeCategory(sourceTypeId) == TYPCATEGORY_STRING 
-     || TypeCategory(targetTypeId) == TYPCATEGORY_STRING) {
-        is_null = fcinfo->isnull;
-        res = try_convert_via_io(value, sourceTypeId, targetTypeId, &is_null);
-    }
+    case CONVERSION_TYPE_ARRAY:
+        elog(ERROR, "no sopport for ARRAY CONVERSION");
+        is_failed = true;
+        break;
+
+    case CONVERSION_TYPE_NONE:
+        fcinfo->isnull = fcinfo->argnull[1];
+        res = fcinfo->arg[1];
+        
+        break;
     
-    if (is_null) {
+    default:
+        /// TODO RAISE ERROR
+        elog(ERROR, "unrecognized conversion method: %d",
+						 (int) conversion_type);
+        break;
+    }
+
+    if (is_failed) {
         fcinfo->isnull = fcinfo->argnull[1];
         res = fcinfo->arg[1];
     }
